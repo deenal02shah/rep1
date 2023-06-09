d3d2a5da1c1c4cb7a596ee1094e34ef61f255b46
deenal02shah <135946087+deenal02shah@users.noreply.github.com>
2023-06-09 19:03:19 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,20 @@ import java.util.*;
 class Main{
 public static void main(String args[]){
 
+  int n=3;
+  
+for(int i=0;i<n;i++){
+for(int j=1;j<=i;j++){
+  system.out.print(j+" ");
 
+
+
+}
+  system.out.println(" ");
+
+
+
+}
 
 }
 }
