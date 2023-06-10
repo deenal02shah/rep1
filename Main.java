770cee08099d9157938951af43b95887f972c701
deenal02shah <135946087+deenal02shah@users.noreply.github.com>
2023-06-10 08:27:36 +0500

    rep1/mainBranch/Main.java2

--- Main.java
+++ Main.java
@@ -6,16 +6,24 @@ public static void main(String args[]){
   
 for(int i=0;i<n;i++){
 for(int j=1;j<=i;j++){
-  system.out.print(j+" ");
-
-
+  System.out.print(j+" ");
+}
+  
+  System.out.println(" ");
+}
+  
+  
+  
 
 }
-  system.out.println(" ");
+}
+class A{
+void static fun1(){
 
-
+System.out.println("rep1/mainBrach/main.java");
 
 }
 
-}
+
+
 }
